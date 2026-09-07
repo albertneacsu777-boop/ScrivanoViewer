d6fac0c8e2c73a5a5430e34664fc45d4622d1c12
albertneacsu777-boop <albertneacsu777@gmail.com>
2026-09-07 16:52:32 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
 }
 
 android { namespace = "com.example.scrivanoviewer"; compileSdk = 35
